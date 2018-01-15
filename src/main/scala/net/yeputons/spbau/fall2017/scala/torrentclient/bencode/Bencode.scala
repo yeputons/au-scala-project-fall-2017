f0e7c4e73a341beb145868d59fec4d9f9cd337d3
Egor Suvorov <egor.suvorov@gmail.com>
2018-01-15 22:36:42 +0300

    Bencode.BList, Bencode.BDict: implement Seq[] and Map[] interfaces, test it

--- src/main/scala/net/yeputons/spbau/fall2017/scala/torrentclient/bencode/Bencode.scala
+++ src/main/scala/net/yeputons/spbau/fall2017/scala/torrentclient/bencode/Bencode.scala
@@ -1,10 +1,47 @@
 package net.yeputons.spbau.fall2017.scala.torrentclient.bencode
 
+import scala.collection.{MapLike, SeqLike, mutable}
+
 sealed trait BEntry
 case class BByteString(value: Seq[Byte]) extends BEntry
+
 case class BNumber(value: Long) extends BEntry
-case class BList(value: BEntry*) extends BEntry
-case class BDict(value: Map[Seq[Byte], BEntry]) extends BEntry
+
+case class BList(value: BEntry*)
+    extends BEntry
+    with Seq[BEntry]
+    with SeqLike[BEntry, BList] {
+  override def apply(idx: Int): BEntry = value(idx)
+
+  override def iterator: Iterator[BEntry] = value.iterator
+
+  override def length: Int = value.length
+
+  override protected[this] def newBuilder: mutable.Builder[BEntry, BList] =
+    Seq.newBuilder.mapResult { x =>
+      BList(x: _*)
+    }
+}
+
+case class BDict(value: Map[Seq[Byte], BEntry])
+    extends BEntry
+    with Map[Seq[Byte], BEntry]
+    with MapLike[Seq[Byte], BEntry, BDict] {
+  def get(key: String): Option[BEntry] = value.get(key.getBytes("ASCII"))
+
+  def apply(key: String): BEntry = value(key.getBytes("ASCII"))
+
+  override def empty: BDict = BDict.empty
+
+  override def +[V1 >: BEntry](kv: (Seq[Byte], V1)): Map[Seq[Byte], V1] =
+    value + kv
+
+  override def get(key: Seq[Byte]): Option[BEntry] = value.get(key)
+
+  override def iterator: Iterator[(Seq[Byte], BEntry)] = value.iterator
+
+  override def -(key: Seq[Byte]): BDict = BDict(value - key)
+}
 
 object BByteString {
   def fromAsciiString(s: String): BByteString =
