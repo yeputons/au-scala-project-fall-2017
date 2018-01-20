cec9e3ba71d8616ba619be940c12f383128c089d
Egor Suvorov <egor.suvorov@gmail.com>
2018-01-20 18:13:14 +0300

    PeerProtocol: add scaladocs

--- src/main/scala/net/yeputons/spbau/fall2017/scala/torrentclient/peer/PeerProtocol.scala
+++ src/main/scala/net/yeputons/spbau/fall2017/scala/torrentclient/peer/PeerProtocol.scala
@@ -10,6 +10,9 @@ import net.yeputons.spbau.fall2017.scala.torrentclient.peer.PeerHandshake.Handsh
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
+/**
+  * Common ancestor for all BitTorrent peer protocol messages (see BEP 3).
+  */
 sealed trait PeerMessage
 object PeerMessage {
   case object KeepAlive extends PeerMessage
@@ -29,6 +32,16 @@ object PeerMessage {
 }
 
 object PeerProtocol {
+
+  /**
+    * Creates [[BidiFlow]] which fully implements BitTorrent peer protocol
+    * via TCP, including handshake and framing. `I1` and `O2` are streams of
+    * [[PeerMessage]], `I2`/`O1` are chunks of data from TCP protocol.
+    *
+    * Materializes to a `Future[HandshakeCompleted.type]` which completes right
+    * after handshake is successfully completed (even if no data exchange follows),
+    * and fails whenever the stream fails.
+    */
   def apply(infoHash: ByteString,
             myPeerId: ByteString,
             otherPeerId: Option[ByteString])
@@ -45,6 +58,13 @@ object PeerProtocol {
 object PeerMessagesParsing {
   import PeerMessage._
 
+  /**
+    * Creates [[BidiFlow]] which implements BitTorrent peer protocol.
+    * `I1` and `O2` are streams of [[PeerMessage]] and `I2`/`O1` are
+    * frames, which should be further translated into TCP bytes via
+    * [[PeerFraming]].
+    * @return
+    */
   def apply()
     : BidiFlow[PeerMessage, ByteString, ByteString, PeerMessage, NotUsed] =
     BidiFlow.fromFunctions(encodeMessage, decodeMessage)
@@ -152,6 +172,13 @@ case class PeerProtocolDecodeException(message: String, frame: ByteString)
 object PeerFraming {
   final val MaximalFrameSize = 1 << 24
 
+  /**
+    * Creates a [[BidiFlow]] which implements BitTorrent peer framing protocol.
+    * Assumes that frames go to `I1`/come out of `O2`, and TCP chunks
+    * go to `I2`/come out of `O2`. Assumes that handshake is already performed
+    * and corresponding bytes will never occur.
+    * @return
+    */
   def apply()
     : BidiFlow[ByteString, ByteString, ByteString, ByteString, NotUsed] =
     Framing.simpleFramingProtocol(MaximalFrameSize)
@@ -163,6 +190,19 @@ object PeerHandshake {
   private final val SupportedExtension: ByteString =
     ByteString(0, 0, 0, 0, 0, 0, 0, 0)
 
+  /**
+    * Creates a [[BidiFlow]] which performs handshake with a remote peer.
+    * Assumes that local client is connected to `I1` and `O2`, and the remote
+    * client is connected to `O1` and `I2`. Each flow is a chunked stream of
+    * bytes, exact chunking does not matter.
+    *
+    * Fails with [[HandshakeCompleted]]  if handshake fails, otherwise passes
+    * data as-is until completion. Other exceptions are also possible.
+    *
+    * Materializes to a `Future[HandshakeCompleted.type]` which completes right
+    * after handshake is successfully completed (even if no data exchange follows),
+    * and fails whenever the stream fails.
+    */
   def apply(infoHash: ByteString,
             myPeerId: ByteString,
             otherPeerId: Option[ByteString])
@@ -202,6 +242,10 @@ object PeerHandshake {
     BidiFlow.fromFlowsMat(localToRemoteFlow, remoteToLocalFlow)(Keep.right)
   }
 
+  /**
+    * A singleton which is signalled by [[PeerHandshake.apply]] once handshake
+    * is successfully performed.
+    */
   case object HandshakeCompleted
 
   case class HandshakeException(message: String) extends Exception(message)
